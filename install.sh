ee7384a5a6181595a4ca6781188f1b665f05ae04
root <sb2@neosys.com>
2023-09-20 08:44:30 +0000

    = SB: Add new tests on extension creation

--- install.sh
+++ install.sh
@@ -2,10 +2,18 @@
 set -euxo pipefail
 
 	apt update
-	apt install cmake git postgresql
-	apt install postgresql-server-dev-all
+	apt install cmake git postgresql postgresql-server-dev-all
+
 	rm build -rf
 	mkdir build
+
 	cmake . -B build
 	cmake --build build
 	cmake --install build
+:
+: The extension is now available to the above postgres installation.
+:
+: Exodus installation will load the extension into each database using
+: "create extension pgexodus;"
+: A quick self test will be performed as part of every loading.
+:
