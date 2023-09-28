946e81d6ba06ace9d3fcc19a23f8a8e25072203a
neo <sb2@neosys.com>
2023-09-28 22:30:06 +0400

    - SB: Compiles under postgres 16 after PG group split out varattr.h from postgres.h

--- src/pgexodus.h
+++ src/pgexodus.h
@@ -1,4 +1,3 @@
-
 // Make sure PG MAGIC BLOCK is present in the .so file
 //
 //  nm pgexodus.so|grep "T Pg_magic_func"
@@ -6,16 +5,6 @@
 // Otherwise when trying to load functions:
 //
 //  ERROR:  incompatible library "/usr/lib/postgresql/12/lib/pgexodus.so": missing magic block
-
-/*
-severely hacked to get around problem building in VS2005
-http://www.mail-archive.com/pgsql-general@postgresql.org/msg116145.html
-remove w64 compatibility compilation option
-and change
-static const Pg_magic_struct
-to
-static Pg_magic_struct
-*/
 
 /*
 
@@ -30,7 +19,8 @@ static Pg_magic_struct
 * With MinGW use postgresql/include/server/port/win32 .
 */
 
-/*
+/* TODO replace atof used in extract_number.c and extract_datetime.c with something faster and not accepting leading spaces etc.
+
 double atof (const char* str);
 
 Convert string to double
@@ -69,124 +59,34 @@ If the converted value would be out of the range of representable values by a do
 it causes undefined behavior. See strtod for a more robust cross-platform alternative when this is a possibility.
 */
 
-#if defined(_MSC_VER) || defined(__MINGW32__)
-#ifndef _USE_32BIT_TIME_T
-#ifndef _WIN64
-#define _USE_32BIT_TIME_T
-#endif
-//to avoid the following warnings that should be removed in postgres 9.1 or 9.2
-//c:\program files (x86)\postgresql\9.0\include\server\pg_config_os.h(106): warning C4005: 'EIDRM' : macro redefinition
-//          C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\include\errno.h(103) : see previous definition of 'EIDRM'
-#pragma warning (disable: 4005)
-#endif
-
-//to avoid the following errors in postgres.h below
-//error C2011: 'timezone' : 'struct' type redefinition	d:\program files\postgresql\8.3\include\server\pg_config_os.h	188	
-//error C2011: 'itimerval' : 'struct' type redefinition	d:\program files\postgresql\8.3\include\server\pg_config_os.h	197	
-#ifndef WIN32
-#define WIN32
-#endif
-#endif
-
-/* BUILDING_DLL causes the declarations in Pg's headers to be declared
-* __declspec(dllexport) which will break DLL linkage. */
-#ifdef BUILDING_DLL
-#error Do not define BUILDING_DLL when building extension libraries
-#endif
-
-/* Ensure that Pg_module_function and friends are declared __declspec(dllexport) */
-#ifndef BUILDING_MODULE
-#define BUILDING_MODULE
-#endif
-
-#ifdef __cplusplus
-extern "C"
-{
-#endif
+///* Ensure that Pg_module_function and friends are declared __declspec(dllexport) */
+//#ifndef BUILDING_MODULE
+//#define BUILDING_MODULE
+//#endif
 
 #include <stdio.h>
 #include <string.h>
 
 #include <postgres.h>
+#if __has_include(<varatt.h>)
+//VARSIZE etc. was split out from postgres.h in Postgres V16+
+#include <varatt.h>
+#endif
 #include <fmgr.h>
 
-/*#include "utils/geo_decls.h"*/
 #include <utils/timestamp.h> //for PG_RETURN_TIMESTAMP
 #include <utils/date.h> //for PG_RETURN_TIME_ADT
 
 #ifndef int4
 #define int4 int32
 #endif
-int4 pg_atoi(char*,int4,int4);
-
-/*backward compatible to pre 8.3 with no SET_VARSIZE*/
-#ifndef SET_VARSIZE
-#define SET_VARSIZE(ret,size) VARATT_SIZEP((ret) ) = (size)
-#endif
-
-#ifdef _MSC_VER
-
-/*--------------- BEGIN REDEFINITION OF PG MACROS -------------------
-*
-* These rewritten versions of PG_MODULE_MAGIC and PG_FUNCTION_INFO_V1
-* declare the module functions as __declspec(dllexport) when building
-* a module. They also provide PGMODULEEXPORT for exporting functions
-* in user DLLs.
-*/
-#undef PG_MODULE_MAGIC
-#undef PG_FUNCTION_INFO_V1
-
-/* This might want to go somewhere other than fmgr.h, like
-* pg_config_os.h alongside the definition of PGDLLIMPORT
-*/
-#if defined(_MSC_VER) || defined(__MINGW32__)
-#if defined(BUILDING_MODULE)
-#define PGMODULEEXPORT __declspec (dllexport)
-#else
-// Never actually used
-#define PGMODULEEXPORT __declspec (dllimport)
-#endif
-#else
-#define PGMODULEEXPORT
-#endif
-
-#define PG_MODULE_MAGIC \
-PGMODULEEXPORT Pg_magic_struct * \
-PG_MAGIC_FUNCTION_NAME(void) \
-{ \
-static Pg_magic_struct Pg_magic_data = PG_MODULE_MAGIC_DATA; \
-return &Pg_magic_data; \
-} \
-extern int no_such_variable
-
-#define PG_FUNCTION_INFO_V1(funcname) \
-PGMODULEEXPORT const Pg_finfo_record * \
-CppConcat(pg_finfo_,funcname) (void) \
-{ \
-static const Pg_finfo_record my_finfo = { 1 }; \
-return &my_finfo; \
-} \
-extern int no_such_variable
-#endif
-/*--------------- END REDEFINITION OF PG MACROS -------------------*/
-
+//int4 pg_atoi(char*,int4,int4);
 
 //TODO check all pallocs for success
 
 //see http://www.postgresql.org/docs/8.3/interactive/xfunc-c.html
 
 //text_extract2 is like text_extract but returns NULL for empty strings
-
-/* POSTGRES INTERFACE
-	PG_ARGISNULL(0) tells you if passed a null
-	PG_GETARG_TEXT_P(n) gives you a pointer to the data structure of parameter n
-	VARDATA() gives you a pointer to the data region of a struct.
-	VARSIZE() gives you the total size of the structure
-	VARHDRSZ
-	//elog(WARNING, "sizeof picktime8: %d",sizeof(picktime8));
-	elog(ERROR, "mvExtractDate cannot convert more than 20 characters to an integer");
-	PG_RETURN_NULL();
-*/
 
 /* DEBUG USING SOMETHING LIKE THIS
 		elog(ERROR, "Debug point xxx");
@@ -195,78 +95,8 @@ extern int no_such_variable
 		PG_RETURN_NULL();
 */
 
-/* POSTGRES SQL FUNCTION DEFINITION CREATION COPY AND PASTE
-
-NB public functions need to be listed in the .def file on windows.
-
-"STRICT" KEYWORD MEANS THAT THE FUNCTION WILL NOT BE CALLED IF ANY OF THE ARGUMENTS ARE NULL
-Note that select statements from exodus are (currently) generated with coalesce function
- to turn nulls into "" in order to retrieve non-existent join records.
-
-LOAD 'pgexodus' without the .dll or .so to load or reload a shared library file NB single quotes
-but the above will not allow simple unload or updating the DLL without stopping and restarting postgres on win32
-
--- All functions are defined as IMMUTABLE although exodus_call could theorectically return anything.
--- First three are not STRICT and return "" in case passed a null
-
--- cut and paste the following SQL to register the functions into postgres --
-
-CREATE OR REPLACE FUNCTION exodus_count(text, text) RETURNS integer
-AS 'pgexodus', 'exodus_count' LANGUAGE C IMMUTABLE;
-
-CREATE OR REPLACE FUNCTION exodus_extract_text(text, int4, int4, int4) RETURNS text
-AS 'pgexodus', 'exodus_extract_text' LANGUAGE C IMMUTABLE;
-
-CREATE OR REPLACE FUNCTION exodus_extract_sort(text, int4, int4, int4) RETURNS text
-AS 'pgexodus', 'exodus_extract_sort' LANGUAGE C IMMUTABLE;
-
--- Remaining functions are STRICT therefore never get called with NULLS
--- also return NULL if passed zero length strings
-
-CREATE OR REPLACE FUNCTION exodus_extract_text2(text, int4, int4, int4) RETURNS text
-AS 'pgexodus', 'exodus_extract_text2' LANGUAGE C IMMUTABLE STRICT;
-
-CREATE OR REPLACE FUNCTION exodus_extract_date(text, int4, int4, int4) RETURNS date
-AS 'pgexodus', 'exodus_extract_date' LANGUAGE C IMMUTABLE STRICT;
-
-CREATE OR REPLACE FUNCTION exodus_extract_time(text, int4, int4, int4) RETURNS time
-AS 'pgexodus', 'exodus_extract_time' LANGUAGE C IMMUTABLE STRICT;
-
-CREATE OR REPLACE FUNCTION exodus_extract_datetime(text, int4, int4, int4) RETURNS timestamp
-AS 'pgexodus', 'exodus_extract_datetime' LANGUAGE C IMMUTABLE STRICT;
-
-CREATE OR REPLACE FUNCTION exodus_extract_number(text, int4, int4, int4) RETURNS float8
-AS 'pgexodus', 'exodus_extract_number' LANGUAGE C IMMUTABLE STRICT;
-
--- CREATE OR REPLACE FUNCTION exodus_call(text, text, text, text, text, int4, int4) RETURNS text
--- AS 'pgexodus', 'exodus_call' LANGUAGE C IMMUTABLE;
-
--- CREATE OR REPLACE FUNCTION exodus_extract_bytea(bytea, int4, int4, int4)
--- RETURNS bytea AS 'pgexodus', 'exodus_extract_bytea' LANGUAGE C IMMUTABLE;
-
-TO REMOVE THE ABOVE DO THE FOLLOWING AS POSTGRES SUPERUSER CONNECTED TO EXODUS DATABASE
-
-drop FUNCTION exodus_count(text, text) cascade;
-drop FUNCTION exodus_extract_text(text, int4, int4, int4) cascade;
-drop FUNCTION exodus_extract_sort(text, int4, int4, int4) cascade;
-drop FUNCTION exodus_extract_text2(text, int4, int4, int4) cascade;
-drop FUNCTION exodus_extract_date(text, int4, int4, int4) cascade;
-drop FUNCTION exodus_extract_time(text, int4, int4, int4) cascade;
-drop FUNCTION exodus_extract_datetime(text, int4, int4, int4) cascade;
-drop FUNCTION exodus_extract_number(text, int4, int4, int4) cascade;
--- drop FUNCTION exodus_call(text, text, text, text, text, int4, int4) cascade;
--- drop FUNCTION exodus_extract_bytea(text, int4, int4, int4) cascade;
-
-*/
-
-/* evade the following warnings from GETINPUTSATRTLENGTH macro
-  but it isnt clear why the warning doesnt come in all places that the macro is called
-pgexodus.c: In function 'exodus_extract_sort': (GETINPUTSTARTLENGTH)
-pgexodus.c:679: warning: 'input' may be used uninitialized in this function
-pgexodus.c: In function 'exodus_extract_text': (GETINPUTSTARTLENGTH)
-pgexodus.c:462: warning: 'input' may be used uninitialized in this function
-*/
-
+// Call extract.c on data, fn, vn, sn and return the start and length of the desired field/value/subvalue
+//
 #define GETINPUTSTARTLENGTH\
 	text *input;\
 	int32 outstart;\
@@ -289,10 +119,5 @@ pgexodus.c:462: warning: 'input' may be used uninitialized in this function
 		subvalueno = PG_GETARG_INT32(3);\
 		extract(VARDATA(input), (int)VARSIZE(input)-VARHDRSZ, (int)fieldno, (int)valueno, (int)subvalueno, &outstart, &outlen);\
 	}\
-
-//extern "C" {
-
-//bool callexodus(const char* serverid, const char* request, const int nrequestbytes, const char* response, int& nresponsebytes);
-//bool callexodus(const char* serverid, const char* request, const int nrequestbytes, const char* response, int* nresponsebytes);
 
 void extract(char * instring, int inlength, int fieldno, int valueno, int subvalueno, int* outstart, int* outlength);
